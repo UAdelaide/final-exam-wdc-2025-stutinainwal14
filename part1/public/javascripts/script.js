db099d59d443c4649dfae31f7528452bc061e10c
stutinainwal14 <nainwalstuti1402@gmail.com>
2025-06-20 05:26:46 +0000

    [automatic commit] 2025-06-20 05:26:46 UTC

---
+++ part1/public/javascripts/script.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue;
+
+    createApp({
+      data() {
+        return {
+          dogImage: ''
+        }
+      },
+      mounted() {
+        this.getDog();
+      },
+      methods: {
+        async getDog() {
+          const res = await fetch('https://dog.ceo/api/breeds/image/random');
+          const data = await res.json();
+          this.dogImage = data.message;
+        }
+      }
+    }).mount('#app');
