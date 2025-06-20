571f44973b42eaf128922e1b6dc0cc5326dd0d8a
stutinainwal14 <nainwalstuti1402@gmail.com>
2025-06-20 11:31:22 +0000

    [automatic commit] 2025-06-20 11:31:22 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
         d.name AS dog_name,
         d.size,
         u.username AS owner_name,
-        CONCAT(l.location_name, ', l.city, ',  l.state, ', ', l.country) AS location
+        CONCAT(l.location_name, ', ', l.city, ', ', l.state, ', ', l.country) AS location
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
