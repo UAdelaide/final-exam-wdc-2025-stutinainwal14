c5452c3450ac1db24906497a862c10358c1194d0
stutinainwal14 <nainwalstuti1402@gmail.com>
2025-06-20 04:57:17 +0000

    [automatic commit] 2025-06-20 04:57:17 UTC

--- part1/app.js
+++ part1/app.js
@@ -6,10 +6,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
+var app = express();
 
 app.use('/users', apiRouter);
 app.use('/api', apiRouter);
-var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
